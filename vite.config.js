7ac1163a351aa40d0a83eb2823e149597d21d93d
Irvin meunier <meunier.irvin@orange.fr>
2026-01-19 23:35:10 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Le champ 'base' doit correspondre au nom de ton dépôt GitHub
+  // Si ton dépôt s'appelle 'tp-taskify', laisse '/tp-taskify/'
+  // N'oublie pas les slashes au début et à la fin !
+  base: '/tp-taskify/', 
+  
   plugins: [react()],
-  base: '/tp-taskify/', // CETTE LIGNE EST ESSENTIELLE
 })
